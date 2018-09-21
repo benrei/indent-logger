f80d44b70d5e20ea1311702ec91a02b76e6c5e53
Benjamin Reiersølmoen <benjamin.reiersolmoen@traftec.no>
2018-09-21 08:44:28 +0200

    added object support

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const indent = (indent, str)=>{
+  if(typeof str === 'object') str = '\n' + JSON.stringify(str, null, 2);
   if(str){
     const log = `${" ".repeat(indent)}${str}`;
     console.log(log);
   }
 };
-module.exports.log = log;
+module.exports.indent = indent;
 
 const indentTimestamp = (indent, str)=>{
+  if(typeof str === 'object') str = '\n' + JSON.stringify(str, null, 2);
   if(str){
     const now = new Date();
     const time = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} -> `
@@ -17,6 +19,7 @@ const indentTimestamp = (indent, str)=>{
 module.exports.indentTimestamp = indentTimestamp;
 
 const timestamp = (str)=>{
+  if(typeof str === 'object') str = '\n' + JSON.stringify(str, null, 2);
   if(str){
     const now = new Date();
     const time = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} -> `
